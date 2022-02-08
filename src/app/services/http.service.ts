118eedf8175a2aaec6f7c5706de99a319ec704d8
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2022-02-07 23:26:11 -0300

    FEAT: Created http service

---
+++ src/app/services/http.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { environment as env } from '../../environments/environment';
+import { APIResponse, Game } from '../models';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HttpService {
+  constructor(private http: HttpClient) {}
+
+  getGameList(
+    ordering: string,
+    search?: string
+  ): Observable<APIResponse<Game>> {
+    let params = new HttpParams().set('ordering', ordering);
+
+    if (search)
+      params = new HttpParams().set('ordering', ordering).set('search', search);
+
+    return this.http.get<APIResponse<Game>>(`${env.BASE_URL}/games`, {
+      params,
+    });
+  }
+}
