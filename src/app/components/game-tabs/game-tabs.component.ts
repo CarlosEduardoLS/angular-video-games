5300e3587b41388ca76cfd98f34fe1c51e01bd7a
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2022-02-10 14:25:58 -0300

    FEAT: Implemented reactivity on game-tabs component

--- src/app/components/game-tabs/game-tabs.component.ts
+++ src/app/components/game-tabs/game-tabs.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Game } from '../../models';
 
 @Component({
   selector: 'app-game-tabs',
   templateUrl: './game-tabs.component.html',
-  styleUrls: ['./game-tabs.component.scss']
+  styleUrls: ['./game-tabs.component.scss'],
 })
 export class GameTabsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  @Input() game: Game;
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
